07f72326384fe1b63b304ac71e8331c7d3efa59b
Miguel Hernández <miguel.hernandez@monterrey.gob.mx>
2024-03-14 10:52:31 -0600

    added files for package documentation, modified settings config and decorators.py

--- src/gdmty-django-users/decorators.py
+++ src/gdmty-django-users/decorators.py
@@ -1,15 +1,13 @@
-from django.conf import settings
 from functools import wraps
 from gdmty_django_recaptcha_enterprise.decorators import requires_recaptcha_token
-
-enabled_recaptcha = getattr(settings, 'ENABLE_RECAPTCHA', False)
+from .settings import ENABLE_RECAPTCHA
 
 
 def recaptcha_verify(action=None):
     def decorator(view_func):
         @wraps(view_func)
         def _wrapped_view(request, *args, **kwargs):
-            if enabled_recaptcha:
+            if ENABLE_RECAPTCHA:
                 return requires_recaptcha_token(action)(view_func)(request, *args, **kwargs)
             else:
                 return view_func(request, *args, **kwargs)
