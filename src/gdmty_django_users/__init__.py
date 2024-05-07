bd1a139d7a07c138c833b7911a6d31d75ef06dc3
cesar-xyz <cdgva@outlook.com>
2024-05-07 11:36:53 -0600

    Correciones de referencias en nombre del paquete.

--- src/gdmty_django_users/__init__.py
+++ src/gdmty_django_users/__init__.py
@@ -1,15 +1,15 @@
 #!-*- coding: utf-8 -*-
 """
-Authentication backend for Django's Administration panel users based on email and password
+Package that is an extension of Django's AbstractBaseUser for user management, using email as user.
 This packages adds features such as group permissions and reCaptcha enterprise token verification.
 
 This package is published as free software under the terms of the Apache License, Version 2.0. Is developed by
 Dirección de Gobierno Digital of the Secretaría de Innovación y Gobierno Abierto of Municipality of Monterrey.
 
 Authors: ['César Benjamín García Martínez <mathereall@gmail.com>', 'Miguel Angel Hernández Cortés
-<miguelhdezc12@gmail.com>', 'César Guillermo Vázquez Álvarez <chechar.2001@gmail.com>']
+<miguelhdezc12@gmail.com>', 'César Guillermo Vázquez Álvarez <cdgva@outlook.com>']
 Email: gobiernodigital@monterrey.gob.mx
-GitHub: https://github.com/gobiernodigitalmonterrey/gdmty-drf-firebase-auth
+GitHub: https://github.com/gobiernodigitalmonterrey/gdmty-django-users
 Package: gdmty_django_users
 PyPi: https://pypi.org/project/gdmty-django-users/
 License: GNU Affero General Public License v3 or later (AGPLv3+)
